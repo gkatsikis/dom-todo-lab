ad70b14ec3b06f3fd1218789b4072c8cc8d18a27
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-02 17:48:10 -0500

    finishing lab beginning

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ const butt = document.querySelector("#submit-button")
 
 const list = document.querySelector("#todo-list")
 
+const reset = document.querySelector("#reset")
+
+const items = document.querySelector(".items")
+
+butt.addEventListener('click', function(evt){
+  const newLi = document.createElement('li')
+  newLi.textContent = inp.value
+  if(inp.value !== "") {
+    document.querySelector('ul').appendChild
+    (newLi)
+    inp.value = ""
+  }
+})
+
+reset.addEventListener('click', function(evt) {
+  inp.value = ""
+  document.removeChild('li')
+})
