00bfbeb7b57ee479c4574f543fb6a4c0f74de296
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-02 17:34:40 -0500

    cached elements being set up

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 console.log('sanity check2')
 
+const inp = document.querySelector("#important")
+
+const butt = document.querySelector("#submit-button")
+
+const list = document.querySelector("#todo-list")
+
